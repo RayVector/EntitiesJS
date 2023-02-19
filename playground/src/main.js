9d8007a8e1e9f5edb2ff0739ea0401a0a6ea50e7
Ray <rayvectorspqr@yahoo.com>
2023-02-20 02:40:01 +0400

    playground

--- playground/src/main.js
+++ playground/src/main.js
@@ -3,16 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import { Entity, EntityOptions, ApiGqlHooks } from '../../src'
+import { Entity, EntityOptions, ApiRestHooks } from '../../src'
+// import { ApiGqlHooks } from '../../src'
 
 Entity.globalOptions = new EntityOptions({
   api: {
     isEnabled: true, // turn on API
-    baseUrl: 'https://graphqlzero.almansi.me/api', // https://jsonplaceholder.typicode.com', // base url for all API operations
+    baseUrl: 'https://jsonplaceholder.typicode.com', // 'https://graphqlzero.almansi.me/api', // base url for all API operations
     watcherEnabled: false, // auto update values to API
     isLoadingStatesEnabled: true // add loading state on each Entity field
   }
 })
-Entity.apiSchema = new ApiGqlHooks()
+// Entity.apiSchema = new ApiGqlHooks()
+Entity.apiSchema = new ApiRestHooks()
 
 createApp(App).use(store).use(router).mount('#app')
