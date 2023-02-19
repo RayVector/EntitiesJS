f702d872677c850173c9cc531b7bbce153d64c3b
Ray <rayvectorspqr@yahoo.com>
2023-02-20 02:39:46 +0400

    playground

--- playground/src/views/Post.vue
+++ playground/src/views/Post.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    Post {{ $route.params.id }}
-    <br>
-    <br>
-    <div v-if="state.itemToEdit !== null">
-      <input type="text" v-model="state.itemToEdit.title"> <br>
+    <div v-if="state.isPostLoading">
+      ... loading ...
+    </div>
+    <div v-else>
+      Post {{ $route.params.id }}
       <br>
-      <input type="text" v-model="fields.title"> <br>
       <br>
+      <div v-if="state.itemToEdit !== null">
+        <input type="text" v-model="state.itemToEdit.title"> <br>
+        <br>
+        <input type="text" v-model="fields.title"> <br>
+        <br>
+      </div>
     </div>
   </div>
 </template>
@@ -22,6 +27,7 @@ import { $readById } from "../../../src";
 const route = useRoute()
 
 const state = reactive({
+  isPostLoading: false,
   itemToEdit: null
 })
 
@@ -30,10 +36,12 @@ let fields = reactive({
 })
 
 onMounted(async () => {
+  state.isPostLoading = true
   const itemId = Number(route.params.id)
   const item = await $readById(Post, itemId)
   state.itemToEdit = item
   fields = item.createState(fields)
+  state.isPostLoading = false
 })
 
 </script>
--- playground/src/views/Posts.vue
+++ playground/src/views/Posts.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <p
-      style="cursor: pointer"
-      v-for="post in state.postList"
-      :key="post.id"
-      @click="router.push({ name: 'post', params: { id: post.id } })"
-    >
-      {{ post.title }}
-    </p>
+    <div v-if="state.isPostsLoading">
+      ... loading ...
+    </div>
+    <div v-else>
+      <p
+          style="cursor: pointer"
+          v-for="post in state.postList"
+          :key="post.id"
+          @click="router.push({ name: 'post', params: { id: post.id } })"
+      >
+        {{ post.title }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -21,11 +26,14 @@ import { $entityList } from "../../../src";
 const router = useRouter()
 
 const state = reactive({
+  isPostsLoading: false,
   postList: []
 })
 
 onMounted(async () => {
+  state.isPostsLoading = true
   state.postList = await $entityList(Post)
+  state.isPostsLoading = false
 })
 
 </script>
