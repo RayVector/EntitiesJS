a6ce3d28327ae7604102fb33f64aae1ab4aa2abd
Ray <rayvectorspqr@yahoo.com>
2023-02-25 20:21:42 +0400

    gql entity playground removed

--- playground/src/entities/Post.js
+++ playground/src/entities/Post.js
@@ -9,21 +9,8 @@ export default class Post extends Entity {
 
   $options = {
     api: {
-      alias: 'posts', // <-- alias required!
-      gql: {
-        aliasForOne: 'post',
-        variables: {
-          "options": {
-            "paginate": {
-              "page": 1,
-              "limit": 5
-            }
-          }
-        },
-        queryParams: {
-          options: 'PageQueryOptions'
-        }
-      }
+      alias: 'posts',
+      watcherEnabled: true
     }
   }
 
